54c04fc6d5e1bc050fc05f26c45c8d80690d50ac
Leonardo Santos <leonardo.santos@unirio.br>
2014-11-27 13:17:04 -0200

    Modelada tabela edição

--- models/db.py
+++ models/db.py
@@ -14,6 +14,12 @@ if not request.is_local:
     from gluon.contrib.login_methods.ldap_auth import ldap_auth
     auth.settings.login_methods=[ldap_auth(mode='uid',server='ldap.unirio.br', base_dn='ou=people,dc=unirio,dc=br')]
 
+db.define_table('edicao',
+                Field('nome', 'string', notnull=True, required=True, label='Edital*'),
+                Field('dt_inicial', 'date', notnull=True, required=True, label='Data inicial*'),
+                Field('dt_conclusao', 'date', notnull=True, required=True, label="Data final*"),
+                Field('disciplinas_obrigatorias', 'boolean', notnull=True, required=True, label='Mostrar somente disciplinas obrigatórias?*'))
+
 ## configure email
 mail = auth.settings.mailer
 mail.settings.server = 'logging' if request.is_local else 'smtp.gmail.com:587'
