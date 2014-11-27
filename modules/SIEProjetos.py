ad07428ae6984247444117b9a3e4090daca60673
Leonardo Santos <leonardo.santos@unirio.br>
2014-11-27 13:18:19 -0200

    Adicionada classe SIEClassficacoesPrj para interagir com a API na tabela CLASSIFICACOES_PRJ. Adicionados metodos  para salvar projeto e trazer classificações de projetos

--- modules/SIEProjetos.py
+++ modules/SIEProjetos.py
@@ -1,19 +1,54 @@
 from gluon import current
 from unirio.api import UNIRIOAPIRequest
 
-class SIEProjetos(object):
+
+class Xpto(object):
     def __init__(self):
-        self.path = "PROJETOS"
         self.api = UNIRIOAPIRequest(current.kAPIKey)
+
+
+class SIEProjetos(Xpto):
+    def __init__(self):
+        super(SIEProjetos, self).__init__()
+        self.path = "PROJETOS"
+
 
     def getProjetos(self):
         params = {
             'LMIN': 0,
-            'LMAX': 99999
+            'LMAX': 20
         }
         fields = [
             'ID_PROJETO',
-            'TITULO'
+            'TITULO',
+            'TIPO_PUBLICO_TAB',
+            'TIPO_PUBLICO_ITEM',
+            'DT_ALTERACAO'
         ]
-        meuResultado = self.api.performGETRequest(self.path, params)
+        meuResultado = self.api.performGETRequest(self.path, params, fields)
         return meuResultado
+
+    def salvarProjeto(self, projeto):
+        return self.api.performPOSTRequest(self.path, projeto)
+
+
+class SIEClassificacoesPrj(Xpto):
+    def __init__(self):
+        super(SIEClassificacoesPrj, self).__init__()
+        self.path = "CLASSIFICACOES_PRJ"
+
+    def getClassificacoesPrj(self):
+        """
+
+        :rtype : list
+        :return: Essa merda retorna blab lab la
+        """
+        params = {
+            'CLASSIFICACAO_ITEM': 1,
+            'CODIGO': 1
+        }
+        fields = [
+            'ID_CLASSIFICACAO',
+            'DESCRICAO'
+        ]
+        return self.api.performGETRequest(self.path, params, fields).content
