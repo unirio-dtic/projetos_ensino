7bbbefc49ec808c3bb0be9e5dec48a85207363ae
Leonardo Santos <leonardo.santos@unirio.br>
2014-12-23 13:20:41 -0200

    Alterado o id do select de disciplina para COD_DISCIPLINA.

--- static/js/registro.js
+++ static/js/registro.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     if($("#ID_CURSO").val() > 0) {
-        $("#ID_DISCIPLINA").html("<option>Carregando...</option>");
-        ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'ID_DISCIPLINA');
+        $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
+        ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
     }
     $("#ID_CURSO").change(function() {
         if($(this).val() > 0){
-            $("#ID_DISCIPLINA").html("<option>Carregando...</option>");
-            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'ID_DISCIPLINA');
+            $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
+            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
 
         }
     });
     $("#ID_CURSO").keyup(function() {
         if($(this).val() > 0) {
-            $("#ID_DISCIPLINA").html("<option>Carregando...</option>");
-            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'ID_DISCIPLINA');
+            $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
+            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
         }
     });
 });
