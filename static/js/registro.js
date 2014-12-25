4572fc81441146971166483a5cd81dd034c8d1d4
diogommartins <diogo.martins@unirio.br>
2014-12-25 16:59:49 -0200

    Refatoração de código

--- static/js/registro.js
+++ static/js/registro.js
@@ -1,19 +1,13 @@
 $(document).ready(function(){
     if($("#ID_CURSO").val() > 0) {
-        $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
-        ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
+        getDisciplinasHTMLOptions()
     }
-    $("#ID_CURSO").change(function() {
-        if($(this).val() > 0){
-            $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
-            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
-
-        }
-    });
-    $("#ID_CURSO").keyup(function() {
-        if($(this).val() > 0) {
-            $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
-            ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
-        }
+    $("#ID_CURSO").on("change keyup", function(){
+       getDisciplinasHTMLOptions();
     });
 });
+
+function getDisciplinasHTMLOptions(){
+    $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
+    ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
+}
