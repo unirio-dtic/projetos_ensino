2de1097e95234567c18f378c5abc38797dbbb085
Leonardo Santos <leonardo.santos@unirio.br>
2014-12-30 14:50:14 -0200

    Refatoração e inclusão da funcionalidade de atualizar o campo escondido ID_UNIDADE.

--- static/js/registro.js
+++ static/js/registro.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
     if($("#ID_CURSO").val() > 0) {
-        getDisciplinasHTMLOptions()
+        getIdUnidade($(this).val());
+        getDisciplinasHTMLOptions($(this).val());
     }
     $("#ID_CURSO").on("change keyup", function(){
-       getDisciplinasHTMLOptions();
+        if($(this).val() > 0) {
+            getIdUnidade($(this).val());
+            getDisciplinasHTMLOptions($(this).val());
+        } else {
+            $("#COD_DISCIPLINA").html("<option>Selecione o curso</option>");
+        }
+
     });
 });
 
-function getDisciplinasHTMLOptions(){
+function getDisciplinasHTMLOptions(idCurso){
     $("#COD_DISCIPLINA").html("<option>Carregando...</option>");
-    ajax('getDisciplinasHTMLOptions', ['ID_CURSO'], 'COD_DISCIPLINA');
+    $.ajax({
+        type: 'POST',
+        url: 'getDisciplinasHTMLOptions',
+        data: { ID_CURSO: idCurso },
+        success: function(retorno) {
+            $("#COD_DISCIPLINA").html(retorno);
+        }
+    });
+}
+
+function getIdUnidade(idCurso) {
+    $.ajax({
+        type: 'POST',
+        url: 'getIdUnidade',
+        data: { ID_CURSO: idCurso },
+        success: function(retorno) {
+            $("#ID_UNIDADE").val(retorno);
+        }
+    });
 }
