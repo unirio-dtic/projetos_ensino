49b4052d87d9422b26268ee0ca9abd18cbdc4618
diogomartins <diogo.martins@unirio.br>
2015-01-09 14:16:25 -0200

    Criação da tabela bolsas

--- models/db.py
+++ models/db.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-
-
+# coding=utf-8
+from authtools import Edicao
 from gluon.tools import Auth, Service, PluginManager
 
 auth = Auth(db)
 service = Service()
 plugins = PluginManager()
+edicao = Edicao(db)
 
 ## create all tables needed by auth if not custom tables
 auth.define_tables(username=True, signature=False)
@@ -28,7 +28,8 @@ db.define_table(
     Field('dt_conclusao', 'date', notnull=True, required=True, label="Data final de registro*"),
     Field('dt_inicial_projeto', 'date', notnull=True, required=True, label='Data inicial do projeto*'),
     Field('dt_conclusao_projeto', 'date', notnull=True, required=True, label="Data final do projeto*"),
-    Field('disciplinas_obrigatorias', 'boolean', notnull=True, required=True, label='Mostrar somente disciplinas obrigatórias?*'))
+    Field('disciplinas_obrigatorias', 'boolean', notnull=True, required=True, label='Mostrar somente disciplinas obrigatórias?*')
+)
 db.define_table(
     'projetos',
     Field('anexo_nome', 'string', notnull=True),
@@ -40,6 +41,13 @@ db.define_table(
     Field('arquivo', 'upload', uploadfield='arquivo_file'),
     Field('arquivo_file', 'blob'),
     Field('tipo_arquivo_item', 'integer'),
+
+)
+
+db.define_table(
+    'bolsas',
+    Field('id_projeto', 'integer'),
+    Field('quantidade_bolsas', 'integer')
 )
 
 
