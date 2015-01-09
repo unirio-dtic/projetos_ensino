6632f5c557b1654f7161c691c3f433f23eee24b1
diogomartins <diogo.martins@unirio.br>
2015-01-09 14:16:02 -0200

    Atualização de campos das tabelas de acompanhamento

--- modules/tables.py
+++ modules/tables.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from gluon import current
 from sie.SIETabEstruturada import SIETabEstruturada
-from sie.SIEProjetos import SIEParticipantesProjs
+from sie.SIEProjetos import SIEParticipantesProjs, SIEProjetos
 from gluon.html import *
 
 __all__ = [
@@ -12,19 +12,30 @@ __all__ = [
 
 class TableProjetos(object):
     def __init__(self, projetos):
+        """
+
+        :type projetos: list
+        """
         self.projetos = projetos
+
+        projetosIds = [p["ID_PROJETO"] for p in self.projetos]
+        bolsas = current.db(current.db.bolsas.id_projeto.belongs(projetosIds)).select()
+        self.bolsas = {bolsa.id_projeto: bolsa.quantidade_bolsas for bolsa in bolsas}
 
     def arquivos(self, projeto):
         arquivos = current.db(current.db.projetos.id_projeto == projeto["ID_PROJETO"]).select()
         if len(arquivos) > 3:
             n = 1
-        return [A(arquivo["anexo_nome"], _href=URL(f='download', args=arquivo["arquivo"])) for arquivo in arquivos]
+        return UL([A(arquivo["anexo_nome"], _href=URL(f='download', args=arquivo["arquivo"])) for arquivo in arquivos])
 
     def avaliacao(self, projeto):
         try:
             return SIETabEstruturada().descricaoDeItem(projeto["AVALIACAO_ITEM"], projeto["AVALIACAO_TAB"])
         except AttributeError:
             return "Avaliação não cadastrada"
+
+    def bolsa(self, projeto):
+        return str(self.bolsas[projeto["ID_PROJETO"]])
 
 
 class TableAcompanhamento(TableProjetos):
@@ -53,7 +64,7 @@ class TableAcompanhamento(TableProjetos):
 class TableAvaliacao(TableProjetos):
     def __init__(self, projetos):
         super(TableAvaliacao, self).__init__(projetos)
-        self.headers = ("Data de registro", "Num. Processo", "Título", "Arquivos", "Situação", "Avaliação", "Avaliar")
+        self.headers = ("Num. Processo", "Data de registro", "Título", "Arquivos", "Situação", "Avaliação", "Qtd. Bolsas", "Avaliar")
 
     def situacao(self, projeto):
         try:
@@ -65,17 +76,23 @@ class TableAvaliacao(TableProjetos):
         aprovar = {"ID_PROJETO": projeto["ID_PROJETO"], "action": "aprovar"}
         reprovar = {"ID_PROJETO": projeto["ID_PROJETO"], "action": "reprovar"}
 
-        return (
-            A("Aprovar", _id=projeto["ID_PROJETO"], callback=URL('adm', 'avaliacaoAjax', vars=aprovar),
-              target="callback-placeholder"),
-            " | ",
-            A("Reprovar", _id=projeto["ID_PROJETO"], callback=URL('adm', 'avaliacaoAjax', vars=reprovar),
-              target="callback-placeholder"),
-        )
+        if not SIEProjetos.isAvaliado(projeto):
+            uniqueDOMid = "avaliar%d" % projeto["ID_PROJETO"]
+
+            return SPAN(
+                A("Aprovar", _id=projeto["ID_PROJETO"], callback=URL('adm', 'avaliacaoAjax', vars=aprovar),
+                  target=uniqueDOMid),
+                " | ",
+                A("Reprovar", _id=projeto["ID_PROJETO"], callback=URL('adm', 'avaliacaoAjax', vars=reprovar),
+                  target=uniqueDOMid),
+                _id=uniqueDOMid
+            )
+        else:
+            return "Avaliado"
 
     def printTable(self):
         return TABLE(
             THEAD(TR([TH(h) for h in self.headers])),
-            TBODY([TR(p['DT_REGISTRO'], p['NUM_PROCESSO'], p['TITULO'], self.arquivos(p), self.situacao(p),
-                      self.avaliacao(p), self.avaliar(p)) for p in self.projetos if p])
+            TBODY([TR(p['NUM_PROCESSO'], p['DT_REGISTRO'], p['TITULO'], self.arquivos(p), self.situacao(p),
+                      self.avaliacao(p), self.bolsa(p), self.avaliar(p)) for p in self.projetos if p])
         )
