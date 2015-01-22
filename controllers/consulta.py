fc810eb3afedb14ccef7791a9bf08859c71c5b7e
diogomartins <diogo.martins@unirio.br>
2015-01-22 19:09:28 -0200

    Adicionado alerta com email do usuário.

--- controllers/consulta.py
+++ controllers/consulta.py
@@ -15,10 +15,19 @@ def index():
     projetos = [SIEProjetos().getProjeto(projeto["ID_PROJETO"]) for projeto in participacoes.content if projeto['ID_PROJETO'] in ids]
 
     tabela = TableAcompanhamento(participacoes.content, projetos)
+    try:
+        email = api.performGETRequest("V_SERVIDORES_EMAIL", {"ID_PESSOA": session.funcionario['ID_PESSOA']}).content[0]['DESCR_MAIL']
+        alert = "Você receberá alertas de avaliação de projeto no email: %s" % str(email)
+        alert_class = "alert-info"
+    except ValueError:
+        alert = "Seu email não pode ser recuperado. Entre em contato com o setor de cadastro da PROGREPE para normalizar sua situação."
+        alert_class = "alert-danger"
 
     return dict(
         tabela=tabela.printTable(),
-        projetos=projetos
+        projetos=projetos,
+        alert=alert,
+        alert_class=alert_class
     )
 
 
