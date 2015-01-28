bbf9bc7a4083c1264f4e0dba85bad5dd16f51511
diogomartins <diogo.martins@unirio.br>
2015-01-28 09:29:20 -0200

    Refatorações para resolver problema de type detection no PyCharm

--- modules/sie/__init__.py
+++ modules/sie/__init__.py
@@ -4,6 +4,11 @@ __all__ = ["SIEDocumento", "SIEFuncionarios", "SIEProjetos", "SIEFluxos", "SIETa
 
 
 class SIE(object):
-    def __init__(self):
-        self.api = current.api
+    def __init__(self, api=current.api):
+        """
+
+        :type api: unirio.api.apirequest.UNIRIOAPIRequest
+        :param api: UNIRIO API
+        """
+        self.api = api
         self.cacheTime = 86400  # Um dia
