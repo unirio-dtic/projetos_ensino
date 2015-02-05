8ef921b50c86db5ebe8d6e835eafe617a36d19c1
diogomartins <diogo.martins@unirio.br>
2015-02-05 12:23:16 -0200

    Adicionada verificação de autenticação de funcionário

--- modules/authtools.py
+++ modules/authtools.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from sie.SIEFuncionarios import SIEFuncionarioID
 from gluon import current, redirect, URL
 
 __author__ = 'diogomartins'
@@ -16,7 +17,8 @@ class Edicao(object):
             return True
         else:
             current.session.flash = "Você precisa selecionar uma edição."
-            redirect(URL("default", "edicoes", vars={"_next": URL(current.request.controller, current.request.function)}))
+            redirect(
+                URL("default", "edicoes", vars={"_next": URL(current.request.controller, current.request.function)}))
 
 
 class Projeto(object):
@@ -26,5 +28,16 @@ class Projeto(object):
     def requires_projeto(self):
         if current.session.projeto:
             return True
-        else:
-            redirect(URL("consulta", "index"))
+
+
+class Pessoa(object):
+    def __init__(self, db):
+        self.db = db
+
+    def isFuncionario(self):
+        try:
+            if not current.session.funcionario:
+                current.session.funcionario = SIEFuncionarioID(current.session.auth.user.username).getFuncionarioIDs()
+            return True
+        except ValueError:
+            current.session.flash = "Seus dados não foram encontrados. É possível que você não esteja autorizado a acessar este recurso."
