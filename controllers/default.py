7a94401effd676f6fff8d9b5995e5fc8a1306fe0
diogomartins <diogo.martins@unirio.br>
2015-02-09 11:35:37 -0200

    Correção de erro que impossibilitava a utilização do sistema caso não houvesse uma edição válida para uso

--- controllers/default.py
+++ controllers/default.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from sie.SIEFuncionarios import SIEFuncionarioID
 
 
 def index():
@@ -11,22 +10,17 @@ def edicoes():
     from forms import FormEdicoes
 
     session.edicao = None
-    session.funcionario = None
-    form = FormEdicoes().form()
+    try:
+        form = FormEdicoes().form()
 
-    if form.process().accepted:
-        session.edicao = db(db.edicao.id == form.vars.edicao).select().first()
-        try:
-            session.funcionario = SIEFuncionarioID(session.auth.user.username).getFuncionarioIDs()
-        except ValueError:
-            session.flash = "Seus dados não foram encontrados. É possível que você não esteja " \
-                            "autorizado a acessar este recurso."
-            auth.logout()
+        if form.process().accepted:
+            session.edicao = db(db.edicao.id == form.vars.edicao).select().first()
+            if request.vars._next:
+                redirect(request.vars._next)
 
-        if request.vars._next:
-            redirect(request.vars._next)
-
-    return dict(form=form)
+        return dict(form=form)
+    except TypeError:
+        return dict(form=None)
 
 
 def user():
