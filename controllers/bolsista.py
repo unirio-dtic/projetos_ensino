550de1b9012ce9dfcb8ad49f07c81834219cb1a0
diogomartins <diogo.martins@unirio.br>
2015-02-20 15:44:04 -0200

    Adicionada inserção de bolsita e participante bolsista no SIE

--- controllers/bolsista.py
+++ controllers/bolsista.py
@@ -8,7 +8,20 @@ def dados():
     redirect(URL('default', 'index'))
 
 
-@auth.requires(pessoa.isFuncionario())
+@auth.requires(pessoa.isFuncionario() and proj.isCoordenador())
 def ajaxCadastrarParticipante():
-    SIEParticipantesProjs().criarParticipanteAluno(request.vars.ID_PROJETO)
-    return dict()
+    bolsas = db(db.bolsas.id_projeto == request.vars.ID_PROJETO).select().first().quantidade_bolsas
+
+    if not bolsas:
+        return dict(success=False)
+
+    bolsistas = SIEParticipantesProjs().getParticipantes({
+        "ID_PROJETO": request.vars.ID_PROJETO,
+        "FUNCAO_ITEM": 3
+    })
+
+    if not bolsistas or len(bolsistas) < bolsas:
+        aluno = (aluno for aluno in session.alunosPossiveis if aluno['ID_CURSO_ALUNO'] == request.vars.ID_CURSO_ALUNO)
+        if aluno:
+            SIEParticipantesProjs().criarParticipanteBolsista(request.vars.ID_PROJETO, aluno)
+            return dict(success=True)
