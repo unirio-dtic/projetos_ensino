afd7c7b57b7601d5c67bebbf14f50ca4fb02f26f
diogomartins <diogo.martins@unirio.br>
2015-05-20 11:59:59 -0300

    Adicionada condição para que somente possa ser dada presença a meses <= ao atual

--- controllers/presenca.py
+++ controllers/presenca.py
@@ -9,6 +9,7 @@ from sie.SIEBolsistas import SIEBolsistas
 def index():
     locale.setlocale(locale.LC_ALL, 'pt_BR')
     meses = {mes: calendar.month_name[mes] for mes in range(1, 13)}
+    mes_atual = date.today().month
 
     try:
         bolsistas = api.performGETRequest('V_BOLSISTAS_ATIVOS_DADOS', {'ID_PROJETO': request.vars.ID_PROJETO}).content
@@ -20,17 +21,20 @@ def index():
 
         def _presenca_btn(mes, bolsista):
             if mes not in presencas[bolsista['ID_BOLSISTA']]:
-                uid = "%i%i" % (mes, bolsista['ID_BOLSISTA'])
-                return TD(A("Dar presença",
-                            target=uid,
-                            _class="btn",
-                            callback=URL('presenca', 'ajaxAdicionarPresenca', vars={'ID_BOLSISTA': bolsista['ID_BOLSISTA'],
-                                                                                    'mes': mes,
-                                                                                    'ano': datetime.today().year})),
-                          _id=uid
-                )
+                if mes <= mes_atual:
+                    uid = "%i%i" % (mes, bolsista['ID_BOLSISTA'])
+                    return TD(A(T("Dar presença"),
+                                target=uid,
+                                _class="btn",
+                                callback=URL('presenca', 'ajaxAdicionarPresenca', vars={'ID_BOLSISTA': bolsista['ID_BOLSISTA'],
+                                                                                        'mes': mes,
+                                                                                        'ano': datetime.today().year})),
+                              _id=uid
+                    )
+                else:
+                    return T("Presença bloqueada")
             else:
-                return "Presente"
+                return T("Presente")
 
         rows = [TR(TD(mes_nome), *[_presenca_btn(mes_num, b) for b in bolsistas]) for mes_num, mes_nome in meses.iteritems()]
 
@@ -51,6 +55,9 @@ def index():
 def ajaxAdicionarPresenca():
     bolsista = SIEBolsistas().getBolsista(request.vars.ID_BOLSISTA)
 
+    if request.vars.mes > date.today().month:
+        return T("Presença bloqueada. Você não pode prever o futuro!")
+
     if proj.isCoordenador(bolsista['ID_PROJETO']):
         try:
             db.presencas.insert(
@@ -60,8 +67,8 @@ def ajaxAdicionarPresenca():
                 mes=request.vars.mes,
                 user_id=auth.user_id
             )
-            return "Presença adicionada"
+            return T("Presença adicionada")
         except Exception as e:
-            return "Um erro ocorreu ao inserir a presença."
+            return T("Um erro ocorreu ao inserir a presença.")
 
-    return "Você não possui permissão para acessar este recurso"
+    return T("Você não possui permissão para acessar este recurso")
